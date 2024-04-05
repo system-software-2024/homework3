5a5e78507143fc697d44e66fe5dcce56b9a80b6b
LaurenceYang1218 <laurence.yang1218@gmail.com>
2024-04-05 16:57:48 +0800

    feat: test set_scheduler

---
+++ test_setsched.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sched.h>
+
+#define SCHED_MLQ 7
+
+int main(int argc, char *argv[]) {
+    if (argc < 3) {
+        fprintf(stderr, "usage: ./test_setsched pid prio\n");
+        return 1;
+    }
+    pid_t pid = strtol(argv[1], NULL, 10);
+    long prio = strtol(argv[2], NULL, 10);
+    struct sched_param param = {
+        .sched_priority = prio
+    };
+    int ret = sched_setscheduler(pid, SCHED_MLQ, &param);
+    if (ret != 0) {
+        perror("sched_setscheduler");
+    }
+    return 0;
+}
